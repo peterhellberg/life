4685b9487da9e3209276546a3c703eb78548f6a3
Peter Hellberg <peter@c7.se>
2015-07-10 11:15:09 -0600

    Added the Loafer (c/7 orthogonal spaceship)
    http://www.conwaylife.com/wiki/Loafer

--- life/life.go
+++ life/life.go
@@ -126,6 +126,8 @@ func Run(variant *string) {
 					case 's':
 						autoRun = false
 						tick(variant)
+					case 'o':
+						drawPattern(&loafer)
 					case 'p':
 						drawPattern(&rPentomino)
 					case 'r':
